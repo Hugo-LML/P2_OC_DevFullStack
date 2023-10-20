ce3a7a1dec174d848b042a1dd3df4b98cd878bf3
Hugo Lemerle <hlml200294@gmail.com>
2023-10-20 09:46:45 +0200

    fix(interfaces): olympic and participations interface modified

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,5 +1,7 @@
 import { Participation } from "./Participation";
 
-export class Olympic {
-  constructor(public id: number, public country: string, public participations: Participation[]) {}
+export interface Olympic {
+  id: number,
+  country: string,
+  participations: Participation[]
 }
